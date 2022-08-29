cd2f4dbddb764ab097cc4f3cf66c00a2ab36f018
Andrew <andrewleduc88@yahoo.com>
2022-08-29 01:32:51 -0400

    8/29 platforms.h

--- Marlin/src/HAL/platforms.h
+++ Marlin/src/HAL/platforms.h
@@ -37,8 +37,7 @@
   #define HAL_PATH(PATH, NAME) XSTR(PATH/LPC1768/NAME)
 #elif defined(__STM32F1__) || defined(TARGET_STM32F1)
   #define HAL_PATH(PATH, NAME) XSTR(PATH/STM32F1/NAME)
-#elif defined(ARDUINO_ARCH_STM32) || !defined(HAL_STM32)
-  #define HAL_STM32
+#elif defined(ARDUINO_ARCH_STM32) || defined(HAL_STM32)
   #define HAL_PATH(PATH, NAME) XSTR(PATH/STM32/NAME)
 #elif defined(ARDUINO_ARCH_ESP32)
   #define HAL_PATH(PATH, NAME) XSTR(PATH/ESP32/NAME)
