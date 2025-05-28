5457a9518c354939eb6cec24cdf21cf0065dc485
Miguel Risco-Castillo <mriscoc@users.noreply.github.com>
2025-05-28 10:07:39 -0500

    Update version to 2.1.4 and add new PlatformIO configuration files for Ender3S1-F1, Ender3S1-F4, Ender3V2-422-BLT, and Ender3V2-422-MM

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.1.3 MRiscoC"
+#define SHORT_BUILD_VERSION "2.1.4 MRiscoC"
 
 /**
  * Verbose version identifier which should contain a reference to the location
